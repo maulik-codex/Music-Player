e3fb4350d0c919150592e61527bec53d2ced074a
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-17 10:53:51 +0530

    Made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import SideBar from './SideBar';
 import TopArtists from './TopArtists';
 function App() {
-  return (
-    <div style={{ display: 'flex' }}>
-      <SideBar />
-      {/* Your main content goes here */}
-      <div style={{ flex: 1, background: '#181818', minHeight: '100vh', color: '#fff', padding: 24 }}>
-        {/* Example main content */}
-        <h1>Welcome to Groovy!</h1>
-        <p>Your music, playlists, and more.</p>
+    return (
+      <>
+        <SideBar />
         <TopArtists />
-      </div>
-    </div>
-      );  
-      }
-      export default App; 
+      </>
+    );  
+  }
+  export default App; 
