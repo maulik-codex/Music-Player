c9ae77361b65cc855356604f2ce02a05983e284d
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-17 12:35:51 +0530

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-    return (
-      <>
-        
-      </>
-    );  
-  }
-  export default App; 
+const App = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
