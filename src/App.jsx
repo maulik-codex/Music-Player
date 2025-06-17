0d78cde8ab48b058fd6243ce77e53788588d5671
maulik-codex <maulikchhansiya@gmail.com>
2025-06-17 10:57:40 +0530

    tring to resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,7 @@ const App = () => {
       <main className="main-content">
         <div className="container fade-in content-with-background-image">
           <Navbar/>
-          <Typography variant="h6" className="trending-text">Trending New Hits</Typography>
-          <Typography variant="h1" className="song-title">WALLS</Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center', mb: 4 }}>
-            <Typography variant="h5" className="artist-name">Louis Tomlinson</Typography>
-            <Typography variant="body1" className="play-count">63Million Plays</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 20 }}>
-            <Button variant="contained" className="listen-now-button">Listen Now</Button>
-            <Button variant="outlined" className="heart-button">
-              <FavoriteBorderOutlinedIcon />
-            </Button>
-          </Box>
-        </div>
+          </div>
       </main>
     </div>
   )
