27ac7e8085b60538d8a75884f2bcd7163c5afed9
maulik-codex <maulikchhansiya@gmail.com>
2025-06-17 12:12:11 +0530

    resolved the issues in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
-import './App.css'
-import { Button, Typography, Box } from '@mui/material'
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 const App = () => {
   return (
-    <div className="app-container">
-      <main className="main-content">
-        <div className="container fade-in content-with-background-image">
-          <Navbar/>
-          </div>
-      </main>
-    </div>
+    <>
+      
+    </>
   )
 }
 
